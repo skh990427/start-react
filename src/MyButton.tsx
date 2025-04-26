7d8f460e5ec18240ee5a443d8dcb1f1610957311
송광호 <skh990427@naver.com>
2025-04-27 00:16:21 +0900

    컴포넌트간 데이터 공유

--- src/MyButton.tsx
+++ src/MyButton.tsx
@@ -1,29 +1,30 @@
 import { useState } from "react";
 
-function MyButton() {
+interface MyButtonProps {
+    count: number; // count는 숫자 타입
+    onClick: () => void; // onClick은 매개변수가 없고 반환값이 없는 함수
+}
+
+function MyButton({ count, onClick } : MyButtonProps) {
+    return (
+        <button onClick={onClick}>
+            Clicked {count} times
+        </button>
+    );
+}
+
+export default function MyApp() {
     const [count, setCount] = useState(0);
 
     function handleCountClick() {
         setCount(count + 1);
     }
 
-    // function handleClick() {
-    //     alert('You clicked me');
-    // }
-
-    return (
-        <button onClick={handleCountClick}>
-            Click {count} times
-        </button>
-    )
-}
-
-export default function MyApp() {
     return (
         <div>
             <h1>Welcome to my app</h1>
-            <MyButton/><br/>
-            <MyButton/>
+            <MyButton count={count} onClick={handleCountClick}/><br/>
+            <MyButton count={count} onClick={handleCountClick}/>
         </div>
     );
 };
